e9c02ba2e58d3a410773af696cd9a1005b51c5c9
Mike Plouffe <mike@mike-plouffe.com>
2015-11-27 18:07:37 -0700

    Created texture, began crane movement behaviour
    Texture created for crane & components, crate.
    CraneController takes user input and... controls the crane.
    Crane prefab created. Currently takes user input and moves the pulley horizontally, magnet up and down.
    Began MagnetComponent. This will be operated by the CraneController and will pick up crates.

---
+++ Loading Screen Jam/Assets/Scripts/CraneController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraneController : MonoBehaviour
+{
+	public bool canDeploy;
+
+	Transform pulleyTransform;
+	Transform magnetTransform;
+
+	public Transform extremeLeft;
+	public Transform extremeRight;
+
+	public float horizontalSpeed = .025f;
+	public float verticalSpeed = .1f;
+
+	void Start ()
+	{
+		pulleyTransform = GameObject.Find("Pulley").transform;
+		magnetTransform = GameObject.Find("Magnet").transform;
+	}
+	
+	void Update()
+	{
+		pulleyTransform.Translate(Vector2.right * Input.GetAxis("Horizontal") * horizontalSpeed);
+		magnetTransform.Translate(Vector2.up * Input.GetAxis("Vertical") * verticalSpeed);
+
+		if(pulleyTransform.position.x < extremeLeft.position.x)
+			pulleyTransform.position = extremeLeft.position;
+		else if(pulleyTransform.position.x > extremeRight.position.x)
+			pulleyTransform.position = extremeRight.position;
+
+		if(Input.GetAxisRaw("Toggle Magnet") > 0)
+		{
+
+		}
+	}
+}
