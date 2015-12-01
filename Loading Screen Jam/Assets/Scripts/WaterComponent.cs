c3073a18804a36abafb25d7b534544637e8e9e51
Mike Plouffe <mike@mike-plouffe.com>
2015-12-01 01:22:11 -0700

    Water prop movement; fixed angles on crates and magnet
    Water props now have movement. Everything is doing a figure-eight, so
    I'll just go in and fix that real quick... tomorrow.
    
    The fixed angle on crates and magnet has now been removed. There's a lot
    more interaction between crates now and it's a bit more fun. What even
    is fun? Who am I? Why am I here?

--- Loading Screen Jam/Assets/Scripts/WaterComponent.cs
+++ Loading Screen Jam/Assets/Scripts/WaterComponent.cs
@@ -7,6 +7,78 @@ public class WaterComponent: MonoBehaviour
 	private const float drag = 100;
 	public ParticleSystem splash;
 
+
+	#region Props
+	public Transform foreground;
+	public Transform midground;
+	public Transform background;
+
+	public float amplitude_vertical = 0.125f;
+	public float amplitude_horizontal = 0f;
+	public float cycleTime = 5;
+
+	float degrees_x, degrees_y;
+
+	Vector2 fore_position;
+	Vector2 mid_position;
+	Vector2 back_position;
+
+	bool fore_start, mid_start, back_start;
+
+	IEnumerator waterDelay()
+	{
+		if(!fore_start)
+		{
+			fore_start = true;
+			yield return new WaitForSeconds(0.1f);
+		}
+		if(!mid_start)
+		{
+			mid_start = true;
+			yield return new WaitForSeconds(0.1f);
+		}
+		if(!back_start)
+		{
+			back_start = true;
+			yield return new WaitForSeconds(0.1f);
+		}
+		yield break;
+	}
+	#endregion
+
+	private void Start()
+	{
+		fore_position = foreground.transform.position;
+		mid_position = midground.transform.position;
+		back_position = background.transform.position;
+
+		StartCoroutine(waterDelay());
+	}
+
+
+	private void Update()
+	{
+		float deltaTime = Time.deltaTime;
+
+		float degreesPerSecond_y = 360  / cycleTime;
+		float degreesPerSecond_x = degreesPerSecond_y * 2;
+
+		degrees_x = Mathf.Repeat(degrees_x + (deltaTime * degreesPerSecond_x), 360);
+		degrees_y = Mathf.Repeat(degrees_y + (deltaTime * degreesPerSecond_y), 360);
+		float radians_x = degrees_x * Mathf.Deg2Rad;
+		float radians_y = degrees_y * Mathf.Deg2Rad;
+
+		Vector2 offset = new Vector2(amplitude_horizontal * Mathf.Sin(radians_x),
+										amplitude_vertical * Mathf.Sin(radians_y));
+
+		if(fore_start)
+			foreground.position = fore_position + offset * 1.125f;
+		if(mid_start)
+			midground.position = mid_position + offset;
+		if(back_start)
+			background.position = back_position + offset * 1.25f;
+	}
+
 	private void OnTriggerEnter2D(Collider2D collider2d)
 	{
 		if(collider2d.name == "Magnet")	return;
@@ -15,6 +87,7 @@ public class WaterComponent: MonoBehaviour
 		if(!body)	return;
 
 		body.drag = drag;
+		body.fixedAngle = true;
 		body.gameObject.AddComponent<LateDestroyerComponent>();
 
 		foreach(Transform child in body.transform)
