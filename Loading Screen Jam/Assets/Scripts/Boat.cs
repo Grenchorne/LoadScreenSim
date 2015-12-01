31c6cf6518f31f734329584c274e423943491652
Mike Plouffe <mike@mike-plouffe.com>
2015-12-01 00:30:46 -0700

    Environmental Art, boat movement
    Art created: bricks, mag lights, boat, clouds
    Boat movement implemented. Crates need to stick to them.

---
+++ Loading Screen Jam/Assets/Scripts/Boat.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boat : MonoBehaviour
+{
+	public float amplitude_vertical = 0.3f;
+	public float amplitude_horizontal = 0.1f;
+	public float cycleTime = 5;
+	
+	float degrees_y, degrees_x;
+
+	private	Vector2 position;
+
+	private void Start()
+	{
+		position = transform.position;
+	}
+	
+	private void Update()
+	{
+		float deltaTime = Time.deltaTime;
+		
+		float degreesPerSecond_y = 360  / cycleTime;
+		float degreesPerSecond_x = degreesPerSecond_y * 2;
+
+		degrees_x = Mathf.Repeat(degrees_x + (deltaTime * degreesPerSecond_x), 360);
+		degrees_y = Mathf.Repeat(degrees_y + (deltaTime * degreesPerSecond_y), 360);
+		float radians_x = degrees_x * Mathf.Deg2Rad;
+		float radians_y = degrees_y * Mathf.Deg2Rad;
+
+		Vector2 offset = new Vector2(	amplitude_horizontal * Mathf.Sin(radians_x),
+										amplitude_vertical * Mathf.Sin(radians_y));
+		transform.position = position + offset;
+	}
+}
+
