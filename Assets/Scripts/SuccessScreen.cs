ebed79ccb5eec20004e0645d0d06c728479c6a7b
Mike Plouffe <mike@mike-plouffe.com>
2015-12-02 23:48:37 -0700

    SuccessScreen
    Added SuccessScreen class to handle the end of the experience. The user
    can now quit the game or restart.

---
+++ Assets/Scripts/SuccessScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuccessScreen : MonoBehaviour
+{
+	private void Update()
+	{
+		if(Input.GetKey(KeyCode.Q))
+		{
+			Application.Quit();
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;			
+#endif
+		}
+
+		if(Input.GetKey(KeyCode.L))
+		{
+			Application.LoadLevel(0);
+		}
+	}
+}
